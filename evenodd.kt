186104c3de0bcf7218631a59e8c358de7d0fe741
doojoo9999 <parkbr95@naver.com>
2023-12-06 09:51:53 +0900

    new file:   evenodd.kt

---
+++ evenodd.kt
@@ -0,0 +1,11 @@
+class Solution {
+    fun solution(num: Int): String {
+        var answer = 
+        if(num%2 == 0){
+            "Even"
+        } else {
+            "Odd"
+        }
+        return answer
+    }
+}
