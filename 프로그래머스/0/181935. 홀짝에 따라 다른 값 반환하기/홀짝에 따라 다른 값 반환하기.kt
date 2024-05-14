9c739d3323901e4a1c61d17d1deb7f4038a6c946
doojoo9999 <96476393+doojoo9999@users.noreply.github.com>
2024-05-14 22:30:20 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 60.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun solution(n: Int): Int {
+        var answer: Int = 0
+        if(n%2 != 0) {
+            for (i in 0..n) {
+                if(i%2 != 0) {answer += i}
+            }
+        } else {
+            for ( i in 0..n) {
+                if(i%2 == 0) {answer += i * i}
+            }
+        }
+        return answer
+    }
+}
