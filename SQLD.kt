445beecae67a6281b14c1ca0bc52afd3434d981f
br p <parkbr95@naver.com>
2024-05-29 08:49:07 +0900

    ddl, dml, dcl

--- SQLD.kt
+++ SQLD.kt
@@ -1,2 +1,10 @@
 class SQLD {
+
+    DDL - 데이터 정의어
+        - CREATE, ALTER, DROP
+    DML - 데이터 조작어
+        - SELECT, INSERT, UPDATE, DELETE
+    DCL - 데이터 컨트롤어
+        - GRANT REVOKE, COMMIT, ROLLBACK
+
 }
